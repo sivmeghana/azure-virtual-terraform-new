fb7313662857b9fa6603f53e523b6b1da835e82d
sivmeghana <85859524+sivmeghana@users.noreply.github.com>
2022-06-27 20:22:20 +0530

    version change

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.98.0"
+    azurerm = "2.52.0"      
     }
-  }
 }
 
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
